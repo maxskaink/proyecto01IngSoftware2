269113878e799933a7cef7e59e2b44beca7116d5
Miguel Angel Calambas <67810439+maxskaink@users.noreply.github.com>
2024-09-10 19:35:26 -0500

    First commit just with the initial proyect

---
+++ src/main/java/co/edu/unicauca/proeycto01ingsoftware/Proeycto01IngSoftware.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.proeycto01ingsoftware;
+
+/**
+ *
+ * @author calam
+ */
+public class Proeycto01IngSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
